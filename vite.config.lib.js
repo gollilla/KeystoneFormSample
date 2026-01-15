4834010e7946f352f7bcdad2518458f206bfd391
gollilla <soradore2@gmail.com>
2026-01-15 20:06:48 +0900

    fix

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -22,7 +22,7 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [
-        'keystonemc',
+        /^keystonemc(\/.*)?$/,
         '@minecraft/server',
         '@minecraft/server-net',
         '@minecraft/server-ui',
